51c028ab3dd0672a86d55248f676f5eb36cbca1b
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-15 07:36:33 +0300

    0.0.2

---
+++ T/Charge.php
@@ -0,0 +1,7 @@
+<?php
+// 2017-02-15
+namespace Dfe\Spryng\T;
+final class Charge extends TestCase {
+	/** @test */
+	function t00() {}
+}
---
+++ T/TestCase.php
@@ -0,0 +1,4 @@
+<?php
+// 2017-02-15
+namespace Dfe\Spryng\T;
+abstract class TestCase extends \Df\Core\TestCase {}
