56cfc0f3cd9ca6104426d9b046989b74f5df21dd
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-16 13:26:18 +0300

    no message

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -1,58 +1,14 @@
 // 2017-02-16
 define([
-	'df', 'Df_Payment/stripeClone', 'jquery', 'mage/translate'
-], function(df, parent, $, $t) {'use strict'; return parent.extend({
-	// 2017-02-16
-	// @used-by mage2pro/core/Payment/view/frontend/web/template/card.html
-	defaults: {df: {card: {newTemplate: 'Dfe_Spryng/card/new'}}},
-	/**
-	 * 2017-02-16
-	 * @override
-	 * @see mage2pro/core/Payment/view/frontend/web/mixin.js
-	 * @used-by mage2pro/core/Payment/view/frontend/web/template/item.html
-	 * @param {HTMLElement} element
-	 * @param {Object} _this
-	 */
-	dfFormAfterRender: function(element, _this) {
-		this._super(element, _this);
-		this.initDf();
-	},
+	'df', 'Df_Payment/stripeClone'
+], function(df, parent) {'use strict'; return parent.extend({
 	/**
 	 * 2017-02-16
 	 * Does Spryng support any other bank cards besides Visa and MasterCard?
 	 * https://mage2.pro/t/2796
 	 * @returns {String[]}
 	 */
-	getCardTypes: function() {return ['VI', 'MC'];},
-	/**
-	 * 2017-02-16
-	 * @used-by initialize()
-	 * @used-by placeOrder()
-	 * @returns {Promise}
-	*/
-	initDf: df.c(function() {
-		/** @type {Deferred} */
-		var deferred = $.Deferred();
-		var _this = this;
-		/** @type {String} */
-		// 2017-02-16
-		// https://www.google.com/search?q=%22jsclient.js%22+site%3Aspryngpayments.com
-		var lib = this.url('cdn/jsclient.js');
-		require.undef(lib);
-		require([lib], function() {jsclient.injectForm(_this.dfForm().get(0), {
-			card_number_placeholder: $t('Credit Card Number')
-			// 2017-02-16
-			// https://github.com/spryngpayments/prestashop/blob/15819af0/views/templates/hook/payment.tpl#L56-L60
-			,cardstore_url: _this.url('v1/card/')
-			,cvv_placeholder3: $t('CVV')
-			,cvv_placeholder4: $t('CVV')
-			,expiry_placeholder: $t('ММ/YY')
-			,no_style: true
-			,payment_products: 	['card']
-			,'submit_title': $t('Place Order')
-		});});
-		return deferred.promise();
-	}),
+	getCardTypes: function() {return ['VI', 'MC', 'AE', 'JCB', 'DI', 'DN'];},
 
 	/**
 	 * 2017-02-16
