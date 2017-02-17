c05b57a8e173da51e9e154ec5084f65664d76d0e
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-17 08:15:39 +0300

    no message

---
+++ Facade/O.php
@@ -0,0 +1,14 @@
+<?php
+namespace Dfe\Spryng\Facade;
+// 2017-02-17
+final class O extends \Df\StripeClone\Facade\O {
+	/**
+	 * 2017-02-17
+	 * @override
+	 * @see \Df\StripeClone\Facade\O::toArray()
+	 * @used-by \Df\StripeClone\Method::transInfo()
+	 * @param object $o
+	 * @return array(string => mixed)
+	 */
+	function toArray($o) {return null;}
+}
