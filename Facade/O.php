1c4971a0e74d30cdd94879bbeb4624a0adb87c49
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-17 14:47:28 +0300

    0.2.2

--- Facade/O.php
+++ Facade/O.php
@@ -5,7 +5,12 @@ final class O extends \Df\StripeClone\Facade\O {
 	/**
 	 * 2017-02-17
 	 * Spryng PHP SDK declares all the response objects properties as public,
-	 * so @uses get_object_vars() does the job.
+	 * so @uses get_object_vars() (partially) does the job.
+	 * Really, there is no way to get the original API response:
+	 * https://mage2.pro/t/2818
+	 * https://github.com/spryngpayments/php/issues/3
+	 * The get_object_vars($o) returns not the same as the original API response:
+	 * https://mage2.pro/t/2812/2
 	 * @override
 	 * @see \Df\StripeClone\Facade\O::toArray()
 	 * @used-by \Df\StripeClone\Method::transInfo()
