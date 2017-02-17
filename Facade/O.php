fb0315f53d62294d0a662e811cc728f61fcb2aec
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-17 14:41:39 +0300

    0.2.1

--- Facade/O.php
+++ Facade/O.php
@@ -4,11 +4,13 @@ namespace Dfe\Spryng\Facade;
 final class O extends \Df\StripeClone\Facade\O {
 	/**
 	 * 2017-02-17
+	 * Spryng PHP SDK declares all the response objects properties as public,
+	 * so @uses get_object_vars() does the job.
 	 * @override
 	 * @see \Df\StripeClone\Facade\O::toArray()
 	 * @used-by \Df\StripeClone\Method::transInfo()
 	 * @param object $o
 	 * @return array(string => mixed)
 	 */
-	function toArray($o) {return null;}
+	function toArray($o) {return get_object_vars($o);}
 }
