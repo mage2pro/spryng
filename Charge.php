78cdd88d587329ab5c53b34e2a660d9ce58325cd
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-18 07:53:02 +0300

    no message

--- Charge.php
+++ Charge.php
@@ -27,6 +27,10 @@ final class Charge extends \Df\StripeClone\Charge {
 	 */
 	protected function pCharge() {return [
 		'customer_ip' => $this->customerIp()
+		// 2017-02-18
+		// [Spryng][API] It looks like the description of the «merchant_reference»
+		// transaction's parameter is incorrect: https://mage2.pro/t/2842
+		,'merchant_reference' => $this->oii()
 	];}
 
 	/**
