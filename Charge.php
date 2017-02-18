211536314ac869b88d05d1de0b3a1f61064d02fc
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-18 19:36:27 +0300

    no message

--- Charge.php
+++ Charge.php
@@ -33,6 +33,10 @@ final class Charge extends \Df\StripeClone\Charge {
 		// [Spryng][API] It looks like the description of the «merchant_reference»
 		// transaction's parameter is incorrect: https://mage2.pro/t/2842
 		,'merchant_reference' => $this->oii()
+		// 2017-02-15
+		// «The payment product being used»
+		// https://www.spryngpayments.com/documentation/credit-cards/
+		,'payment_product' => 'card'
 		,'user_agent' => df_request_ua()
 	];}
 
