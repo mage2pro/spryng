aae3e9a33c56aedb12188c4b555a32adda90c670
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-18 12:09:24 +0300

    no message

---
+++ T/Customer.php
@@ -0,0 +1,13 @@
+<?php
+// 2017-02-18
+namespace Dfe\Spryng\T;
+use SpryngPaymentsApiPhp\Object\Card as lCard;
+use SpryngPaymentsApiPhp\Object\Customer as lCustomer;
+use SpryngPaymentsApiPhp\Object\Transaction as lCharge;
+final class Customer extends TestCase {
+	/** @test 2017-02-15 */
+	function t00() {}
+
+	/** 2017-02-18 */
+	function t01() {}
+}
