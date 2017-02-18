d5f8fbab7084ce007998e0630a89dd3e37011b8c
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-18 12:22:46 +0300

    no message

--- T/Customer.php
+++ T/Customer.php
@@ -3,11 +3,31 @@
 namespace Dfe\Spryng\T;
 use SpryngPaymentsApiPhp\Object\Card as lCard;
 use SpryngPaymentsApiPhp\Object\Customer as lCustomer;
-use SpryngPaymentsApiPhp\Object\Transaction as lCharge;
 final class Customer extends TestCase {
 	/** @test 2017-02-15 */
 	function t00() {}
 
 	/** 2017-02-18 */
-	function t01() {}
+	function t01() {
+		/** @var lCustomer $customer */
+		$customer = $this->api()->customer->create([
+			'city' => 'Stockholm'
+			,'country_code' => 'SE'
+			,'date_of_birth' => '1982-07-08'
+			,'email_address' => 'admin@mage2.pro'
+			,'first_name' => 'Dmitry'
+			,'gender' => 'male'
+			,'last_name' => 'Fedyuk'
+			,'organisation' => 'Mage2.PRO'
+			,'organisation_name' => 'Mage2.PRO'
+			,'phone_number' => '+46850506000'
+			,'postal_code' => '111 64'
+			,'region' => 'Stockholm'
+			// 2017-02-18
+			// https://mage2.pro/t/2566
+			,'social_security_number' => '410321-9202'
+			,'street_address' => 'Nils Ericsons Plan 4'
+			,'title' => 'mr'
+		]);
+	}
 }
