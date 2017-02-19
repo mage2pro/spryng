28a2bdb22605d842f81ceb9384f3b54142dcb6c6
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-19 14:57:48 +0300

    no message

--- T/Charge.php
+++ T/Charge.php
@@ -4,7 +4,7 @@ namespace Dfe\Spryng\T;
 use SpryngPaymentsApiPhp\Object\Card as lCard;
 use SpryngPaymentsApiPhp\Object\Transaction as lCharge;
 final class Charge extends TestCase {
-	/** 2017-02-15 */
+	/** @test 2017-02-15 */
 	function t00() {}
 
 	/** 2017-02-15 */
@@ -70,10 +70,9 @@ final class Charge extends TestCase {
 	}
 
 	/**
-	 * @test
 	 * 2017-02-19
-	 * [Spryng] An example of the «captureTransaction» API method response
-	 * https://mage2.pro/t/2850
+	 * https://api.spryngpayments.com/v1/#operation/captureTransaction
+	 * [Spryng] An example of the «captureTransaction» API method response: https://mage2.pro/t/2850
 	 */
 	function t03_auth_and_capture_partial() {
 		$amount = 10000;
@@ -99,6 +98,37 @@ final class Charge extends TestCase {
 			,'user_agent' => 'Mage2.PRO'
 		]);
 		$charge = $this->api()->transaction->capture($charge->_id, $amount / 2);
+		echo df_json_encode_pretty($charge);
+	}
+
+	/**
+	 * 2017-02-19
+	 * https://api.spryngpayments.com/v1/#operation/voidAuthTransaction
+	 * [Spryng] An example of the «voidAuthTransaction» API method response: https://mage2.pro/t/2851
+	 */
+	function t04_auth_and_void() {
+		/** @var lCard $card */
+		$card = $this->api()->card->create([
+			'card_number' => '4024007108173153'
+			,'cvv' => '123'
+			,'expiry_month' => '12'
+			,'expiry_year' => '18'
+		]);
+		/** @var lCharge $transaction */
+		$charge = $this->api()->transaction->create([
+			'account' => $this->acccountId()
+			,'amount' => 10000
+			,'card' => $card->_id
+			,'capture_now' => false
+			,'customer_ip' => '127.0.0.1'
+			,'dynamic_descriptor' => 'MAGE2PRO'
+			// 2017-02-15
+			// «The payment product being used»
+			// https://www.spryngpayments.com/documentation/credit-cards/
+			,'payment_product' => 'card'
+			,'user_agent' => 'Mage2.PRO'
+		]);
+		$charge = $this->api()->transaction->void($charge->_id);
 		echo df_json_encode_pretty($charge);
 	}
 }
