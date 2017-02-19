23361ef6fe4b85f944e4df73186b5eeb0dc5121d
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-19 14:29:05 +0300

    no message

--- T/Charge.php
+++ T/Charge.php
@@ -7,8 +7,8 @@ final class Charge extends TestCase {
 	/** 2017-02-15 */
 	function t00() {}
 
-	/** @test 2017-02-15 */
-	function t01() {
+	/** 2017-02-15 */
+	function t01_refund() {
 		/** @var lCard $card */
 		$card = $this->api()->card->create([
 			'card_number' => '4024007108173153'
@@ -29,11 +29,42 @@ final class Charge extends TestCase {
 			,'payment_product' => 'card'
 			,'user_agent' => 'Mage2.PRO'
 		]);
+		$this->api()->transaction->refund($charge->_id, '5000', 'A reason.');
 		/** @var lCard $oCard */
 		$oCard = $this->api()->card->getById($charge->card->_id);
 		echo df_json_encode_pretty($charge);
 		// 2017-02-16
 		// [Spryng] An example of the «createTransaction» API method response
 		// https://mage2.pro/t/2800
+	}
+
+	/** @test 2017-02-19 */
+	function t02_auth_and_capture() {
+		/** @var lCard $card */
+		$card = $this->api()->card->create([
+			'card_number' => '4024007108173153'
+			,'cvv' => '123'
+			,'expiry_month' => '12'
+			,'expiry_year' => '18'
+		]);
+		/** @var lCharge $transaction */
+		$charge = $this->api()->transaction->create([
+			'account' => $this->acccountId()
+			,'amount' => '10000'
+			,'card' => $card->_id
+			,'capture_now' => false
+			,'customer_ip' => '127.0.0.1'
+			,'dynamic_descriptor' => 'MAGE2PRO'
+			// 2017-02-15
+			// «The payment product being used»
+			// https://www.spryngpayments.com/documentation/credit-cards/
+			,'payment_product' => 'card'
+			,'user_agent' => 'Mage2.PRO'
+		]);
+		$charge = $this->api()->transaction->capture($charge->_id);
+		echo df_json_encode_pretty($charge);
+		// 2017-02-19
+		// [Spryng] An example of the «captureTransaction» API method response
+		// https://mage2.pro/t/2850
 	}
 }
