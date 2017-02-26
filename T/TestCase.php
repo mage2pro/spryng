28a33fcfe46ad57c55900cd654d3bc21496f60ad
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-26 13:28:20 +0300

    no message

--- T/TestCase.php
+++ T/TestCase.php
@@ -1,18 +1,18 @@
 <?php
-// 2017-02-15
 namespace Dfe\Spryng\T;
-use Dfe\Spryng\Settings as S;
 use SpryngPaymentsApiPhp\Client as API;
+// 2017-02-15
+/** @method \Dfe\Spryng\Settings s() */
 abstract class TestCase extends \Df\Core\TestCase {
 	/**
 	 * 2017-02-15
 	 * @return string
 	 */
-	final protected function acccountId() {return S::s()->account()->_id;}
+	final protected function acccountId() {return $this->s()->account()->_id;}
 
 	/**
 	 * 2017-02-15
 	 * @return API
 	 */
-	final protected function api() {return S::s()->api();}
+	final protected function api() {return $this->s()->api();}
 }
