76e76a08582b742f18bc8026e9a5e68e5014fe72
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-03 17:59:55 +0300

    no message

--- Source/Account.php
+++ Source/Account.php
@@ -18,7 +18,7 @@ final class Account extends \Df\Payment\Source\API\Key\Testable {
 	 * 2017-02-15
 	 * @override
 	 * @see \Df\Config\Source\API\Key::apiKeyTitle()
-	 * @used-by \Df\Config\Source\API\Key::requirementTitle()
+	 * @used-by \Df\Config\Source\API\Key::requirement()
 	 * @return string
 	 */
 	protected function apiKeyTitle() {return 'an API Key';}
