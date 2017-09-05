c8dd2b31b2574aa963ce6ff6e9f406cb68480865
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-06 02:39:10 +0300

    no message

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -1,7 +1,9 @@
 // 2017-02-16
 define([
 	'df', 'Df_StripeClone/main', 'jquery'
-], function(df, parent, $) {'use strict'; return parent.extend({
+], function(df, parent, $) {'use strict';
+/** 2017-09-06 @uses Class::extend() https://github.com/magento/magento2/blob/2.2.0-rc2.3/app/code/Magento/Ui/view/base/web/js/lib/core/class.js#L106-L140 */	
+return parent.extend({
 	/**
 	 * 2017-02-16
 	 * Does Spryng support any other bank cards besides Visa and MasterCard?
