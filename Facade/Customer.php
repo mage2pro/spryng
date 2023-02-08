9f3e224b5bf32a4c00c3e902197071e4214104bb
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-08 08:02:01 +0000

    https://github.com/mage2pro/core/issues/168

--- Facade/Customer.php
+++ Facade/Customer.php
@@ -57,7 +57,6 @@ final class Customer extends \Df\StripeClone\Facade\Customer {
 
 	/**
 	 * 2017-02-17
-	 * @return API
 	 */
-	private function api() {return $this->m()->api();}
+	private function api():API {return $this->m()->api();}
 }
