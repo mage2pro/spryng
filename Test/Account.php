0990f05594497b2fa61b41e88b70f42f044bcdac
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-08 08:04:56 +0000

    https://github.com/mage2pro/core/issues/168

--- Test/Account.php
+++ Test/Account.php
@@ -4,15 +4,15 @@ namespace Dfe\Spryng\Test;
 use SpryngPaymentsApiPhp\Object\Account as lAccount;
 final class Account extends CaseT {
 	/** 2017-02-15 @test */
-	function t00() {}
+	function t00():void {}
 
 	/** 2017-02-15 */
-	function t01() {
+	function t01():void {
 		/** @var lAccount $accounts */
 		$account = df_first($this->api()->account->getAll());
 		print_r(df_json_encode($account));
 	}
 
 	/** 2017-02-15 */
-	function t02() {print_r(df_json_encode($this->s()->account()));}
+	function t02():void {print_r(df_json_encode($this->s()->account()));}
 }
