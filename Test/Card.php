b3467ac436b6f9ee0e6547ae7bab9fc32d1d83bf
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-08 08:05:43 +0000

    refactoring

--- Test/Card.php
+++ Test/Card.php
@@ -8,13 +8,12 @@ final class Card extends CaseT {
 
 	/** 2017-02-15 */
 	function t01():void {
-		/** @var lCard $card */
 		$card = $this->api()->card->create([
 			'card_number' => '4024007108173153'
 			,'cvv' => '123'
 			,'expiry_month' => '12'
 			,'expiry_year' => '18'
-		]);
+		]); /** @var lCard $card */
 		print_r(df_json_encode($card));
 	}
 }
